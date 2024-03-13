b6b6682407caa7be3d667e57a6f54d9c1c1defa8
padermo <fabioaeh96@gmail.com>
2024-03-13 05:57:42 -0500

    agregando favicon personalizado

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,6 +7,7 @@ import { NextIntlClientProvider, useMessages } from 'next-intl';
 import ScrollTop from '@/components/config/ScrollTop';
 import CV from '@/components/config/CV';
 import Cookies from '@/components/CookiesConsent/Cookies';
+import Favicon from '/public/favicon.ico'
 
 const poppins = Poppins({ weight: ['300', '500', '700'], subsets: ['latin'] });
 
@@ -14,6 +15,7 @@ export const metadata: Metadata = {
   title: 'Fabio Estevez Developer',
   description:
     'Desarrollador Full Stack con enfoque especializado en Frontend y diseño de experiencias de usuario. Amplio dominio de Next.js, JavaScript, Sass, Tailwind y React Native, con 2 años de experiencia, incluyendo 1 año de preparación intensiva y 1 año trabajando como freelance. Apasionado por la creación de interfaces atractivas y funcionales que ofrecen una experiencia de usuario excepcional.',
+  icons: [{rel: 'icon', url:Favicon.src}]
 };
 
 export default function RootLayout({
