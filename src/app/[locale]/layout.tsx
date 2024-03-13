1aed5359060de989042163bc1e9b661c2e32b2b3
padermo <fabioaeh96@gmail.com>
2024-03-13 05:27:20 -0500

    arreglando ThemeContext

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,7 +6,6 @@ import Navbar from '@/components/Navbar/Navbar';
 import { NextIntlClientProvider, useMessages } from 'next-intl';
 import ScrollTop from '@/components/config/ScrollTop';
 import CV from '@/components/config/CV';
-import favicon from './icon.png';
 import Cookies from '@/components/CookiesConsent/Cookies';
 
 const poppins = Poppins({ weight: ['300', '500', '700'], subsets: ['latin'] });
@@ -17,7 +16,7 @@ export const metadata: Metadata = {
     'Desarrollador Full Stack con enfoque especializado en Frontend y diseño de experiencias de usuario. Amplio dominio de Next.js, JavaScript, Sass, Tailwind y React Native, con 2 años de experiencia, incluyendo 1 año de preparación intensiva y 1 año trabajando como freelance. Apasionado por la creación de interfaces atractivas y funcionales que ofrecen una experiencia de usuario excepcional.',
 };
 
-export default function LocaleLayout({
+export default function RootLayout({
   children,
   params: { locale },
 }: Readonly<{
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -7,7 +7,7 @@ export const ThemeContext = createContext({} as Theme)
 
 export default function ThemeProvider({children}:Children){
   const [theme, setTheme] = useState<string>(()=>{
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+    if (typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches){
       return 'dark'
     }
     return 'light'
