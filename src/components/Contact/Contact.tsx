93fddd1bca5991da616d3e3b38cb36b1f4f78a26
padermo <fabioaaeh96@gmail.com>
2024-06-26 14:05:23 -0500

    update: adding mail button in Contact.tsx

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,21 +1,32 @@
-'use client'
-import { GithubFilled, LinkedinFilled } from '@ant-design/icons'
-import { Tooltip } from 'antd'
-import { viewGitHub, viewLinkedIn } from '@/utils/functions'
+"use client";
+import { GithubFilled, LinkedinFilled, MailFilled } from "@ant-design/icons";
+import { Tooltip } from "antd";
+import { viewGitHub, viewLinkedIn } from "@/utils/functions";
 
-export default function Contact(){
-  return(
-    <div className='flex items-center gap-2'>
-      <Tooltip title='GitHub' placement='bottom'>
-        <button onClick={viewGitHub} className='rounded-full flex items-center p-2 text-xl transition-colors duration-300 ease-in-out text-[#222] dark:text-white hover:bg-[#d5d5d5] dark:hover:bg-[#555] hover:shadow-lg hover:text-[#CFB53B]'>
-          <GithubFilled/>
+export default function Contact() {
+  return (
+    <div className="flex items-center gap-2">
+      <Tooltip title="GitHub" placement="bottom">
+        <button
+          onClick={viewGitHub}
+          className="rounded-full flex items-center p-2 text-xl transition-colors duration-300 ease-in-out text-[#222] dark:text-white hover:bg-[#d5d5d5] dark:hover:bg-[#555] hover:shadow-lg hover:text-[#CFB53B]"
+        >
+          <GithubFilled />
         </button>
       </Tooltip>
-      <Tooltip title='LinkedIn' placement='bottom'>
-        <button onClick={viewLinkedIn} className='rounded-full flex items-center p-2 text-xl transition-colors duration-300 ease-in-out text-[#222] dark:text-white hover:bg-[#d5d5d5] dark:hover:bg-[#555] hover:shadow-lg hover:text-[#CFB53B]'>
-          <LinkedinFilled/>
+      <Tooltip title="LinkedIn" placement="bottom">
+        <button
+          onClick={viewLinkedIn}
+          className="rounded-full flex items-center p-2 text-xl transition-colors duration-300 ease-in-out text-[#222] dark:text-white hover:bg-[#d5d5d5] dark:hover:bg-[#555] hover:shadow-lg hover:text-[#CFB53B]"
+        >
+          <LinkedinFilled />
+        </button>
+      </Tooltip>
+      <Tooltip title="fabioaeh96@gmail.com" placement="bottom">
+        <button className="rounded-full flex items-center p-2 text-xl transition-colors duration-300 ease-in-out text-[#222] dark:text-white hover:bg-[#d5d5d5] dark:hover:bg-[#555] hover:shadow-lg hover:text-[#CFB53B]">
+          <MailFilled />
         </button>
       </Tooltip>
     </div>
-  )
+  );
 }
