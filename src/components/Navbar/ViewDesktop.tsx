d11e84f744ac0abc8a5c2c7699fcf5867e3e4759
padermo <fabioaaeh96@gmail.com>
2024-07-04 16:07:08 -0500

    update: modifying anchor movement in navbar

--- src/components/Navbar/ViewDesktop.tsx
+++ src/components/Navbar/ViewDesktop.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useTranslations } from "next-intl";
-import Link from "next/link";
 import SwitchTheme from "../config/SwitchTheme";
 import Lang from "../Lang/Lang";
 import { useSearchParams } from "next/navigation";
@@ -10,29 +9,34 @@ export default function ViewDesktop() {
   const searchParams = useSearchParams();
   const viewPolicyCookies = searchParams.get("view");
 
+  const handleScrollSection = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) element.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <nav
       className={`w-full h-16 absolute top-0 z-10 items-center justify-end px-4 md:px-8 lg:px-10 hidden lg:flex ${viewPolicyCookies && "lg:hidden"}`}
     >
       <div className="flex items-center gap-4 text-[#222] dark:text-white">
-        <Link
-          href={"#about"}
-          className="font-light leading-7 border-b-[#CFB53B] hover:border-b"
+        <label
+          onClick={() => handleScrollSection("about")}
+          className="font-light leading-7 cursor-pointer border-b-[#CFB53B] hover:border-b"
         >
           {t("about")}
-        </Link>
-        <Link
-          href={"#projects"}
-          className="font-light leading-7 border-b-[#CFB53B] hover:border-b"
+        </label>
+        <label
+          onClick={() => handleScrollSection("projects")}
+          className="font-light leading-7 cursor-pointer border-b-[#CFB53B] hover:border-b"
         >
           {t("projects")}
-        </Link>
-        <Link
-          href={"#contact"}
-          className="font-light leading-7 border-b-[#CFB53B] hover:border-b"
+        </label>
+        <label
+          onClick={() => handleScrollSection("contact")}
+          className="font-light leading-7 cursor-pointer border-b-[#CFB53B] hover:border-b"
         >
           {t("contact")}
-        </Link>
+        </label>
         <SwitchTheme />
         <Lang />
       </div>
--- src/components/Projects/images.ts
+++ src/components/Projects/images.ts
@@ -1,6 +1,19 @@
-import type { Images } from '@/types/generals';
+import type { Images } from "@/types/generals";
 
-export const cava:Images = ['https://res.cloudinary.com/dultasdzm/image/upload/v1709845935/portfolio/cava_h24kf5.jpg'];
-export const sanatorio:Images = ['https://res.cloudinary.com/dultasdzm/image/upload/v1709846234/portfolio/sanatorio-1_uo2fcg.jpg', 'https://res.cloudinary.com/dultasdzm/image/upload/v1709846234/portfolio/sanatorio-2_zz0w1r.jpg', 'https://res.cloudinary.com/dultasdzm/image/upload/v1709846235/portfolio/sanatorio-3_p3gb9i.jpg']
-export const printec:Images = ['https://res.cloudinary.com/dultasdzm/image/upload/v1710014276/portfolio/printec-1_wpattj.jpg', 'https://res.cloudinary.com/dultasdzm/image/upload/v1710014276/portfolio/printec-2_u6tg63.jpg']
-export const creative:Images = ['https://res.cloudinary.com/dultasdzm/image/upload/v1713303924/portfolio/zwuchsaxhsbohao4ljep.png', 'https://res.cloudinary.com/dultasdzm/image/upload/v1713303924/portfolio/mpr6c3ndxacnol1hrkfd.png', 'https://res.cloudinary.com/dultasdzm/image/upload/v1713303924/portfolio/c1hp5vcclx6fiddnhcxo.png']
+export const cava: Images = [
+  "https://res.cloudinary.com/dultasdzm/image/upload/v1709845935/portfolio/cava_h24kf5.jpg",
+];
+export const sanatorio: Images = [
+  "https://res.cloudinary.com/dultasdzm/image/upload/v1720124482/portfolio/sanatorio_1_mm2slj.png",
+  "https://res.cloudinary.com/dultasdzm/image/upload/v1720124482/portfolio/sanatorio_2_lls4u1.png",
+  "https://res.cloudinary.com/dultasdzm/image/upload/v1720124482/portfolio/sanatorio-3_f3sxvl.png",
+];
+export const printec: Images = [
+  "https://res.cloudinary.com/dultasdzm/image/upload/v1710014276/portfolio/printec-1_wpattj.jpg",
+  "https://res.cloudinary.com/dultasdzm/image/upload/v1710014276/portfolio/printec-2_u6tg63.jpg",
+];
+export const creative: Images = [
+  "https://res.cloudinary.com/dultasdzm/image/upload/v1713303924/portfolio/zwuchsaxhsbohao4ljep.png",
+  "https://res.cloudinary.com/dultasdzm/image/upload/v1713303924/portfolio/mpr6c3ndxacnol1hrkfd.png",
+  "https://res.cloudinary.com/dultasdzm/image/upload/v1713303924/portfolio/c1hp5vcclx6fiddnhcxo.png",
+];
