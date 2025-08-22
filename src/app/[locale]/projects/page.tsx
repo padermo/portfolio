494fb0595aea15ffbea2c3302d015b901304efd2
padermo <fabioaeh96@gmail.com>
2025-08-21 20:12:32 -0500

    update lenis

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+import { useEffect } from "react";
 import SkillsWithProjects from "@/components/SkillsWithProjects";
 import Navbar from "@/components/Navbar";
+import Lenis from "lenis";
 
 export default function AllProjects() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2, // desplazamiento (tiempo en segundos)
+      lerp: 0.08, // suavizado
+      touchMultiplier: 1.2, //sensibilidad touch
+      smoothWheel: true, // suavizado mouse
+      wheelMultiplier: 1, // sensibilidad mouse
+    });
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <main>
       <Navbar />
