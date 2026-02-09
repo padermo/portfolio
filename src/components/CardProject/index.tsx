ae2510fbd6fbc74de56953a8207db68d918e7f19
padermo <fabioaeh96@gmail.com>
2026-02-09 09:30:15 -0500

    fix(ux): cards responsive

--- src/components/CardProject/index.tsx
+++ src/components/CardProject/index.tsx
@@ -18,28 +18,39 @@ export default function CardProject({ projects, selectedSkill }: Props) {
   return (
     <>
       {projects.map((data: ContentProjects, i: number) => (
-        <article key={i} className="group relative overflow-hidden mb-4 rounded-lg shadow-lg shadow-background/50">
+        <article key={i} className="group relative overflow-hidden mb-8 lg:mb-4 lg:rounded-lg lg:shadow-lg lg:shadow-background/50">
           <Image
             src={`/images/${data.image}.webp`}
             alt={data.title.toLocaleLowerCase()}
             width={1365}
             height={685}
             loading="lazy"
-            className="w-full h-full transition-transform duration-500 ease-in-out group-hover:scale-105"
+            className="w-full h-full transition-transform duration-500 ease-in-out rounded-lg shadow-lg shadow-background/50 lg:group-hover:scale-105 lg:rounded-none lg:shadow-none"
           />
 
-          <div className="absolute transition-transform duration-300 ease-in-out translate-y-full group-hover:translate-y-0 rounded-lg inset-0 h-full p-4 bg-gradient-to-t from-black/60 to-black/60">
+          <div className="lg:absolute transition-transform duration-300 ease-in-out lg:translate-y-full group-hover:lg:translate-y-0 lg:rounded-lg lg:inset-0 lg:h-full p-4 lg:bg-gradient-to-t lg:from-black/60 lg:to-black/60">
             <a
               href={data.url}
               target="_blank"
-              className="absolute top-0 right-0 p-4"
+              className="hidden lg:block absolute top-0 right-0 p-4"
               aria-label={`Ìr al sitio web de ${data.title}`}
               rel="noopener noreferrer"
             >
               {data.url && <External />}
             </a>
-            <div className="absolute left-0 bottom-0 p-4">
-              <h2 className="font-bold font-poppins text-xl text-foreground">{data.title}</h2>
+            <div className="lg:absolute lg:left-0 lg:bottom-0 lg:p-4">
+              <div className="flex justify-between items-center py-2 lg:py-0">
+                <h2 className="font-bold font-poppins text-xl text-foreground">{data.title}</h2>
+                <a
+                  href={data.url}
+                  target="_blank"
+                  className="block lg:hidden"
+                  aria-label={`Ìr al sitio web de ${data.title}`}
+                  rel="noopener noreferrer"
+                >
+                  {data.url && <External />}
+                </a>
+              </div>
               <p className="text-base">{data.paragraph}</p>
               <div className="flex w-full gap-1 flex-wrap justify-start mt-2">
                 {data.skills.map((skill: string, j: number) => (
